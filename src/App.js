c872efc099bb7a3ea227318c60ced018b63f73e2
AliLotfi123 <a.lotfi@outlook.com>
2020-07-20 16:51:33 +0200

    Added MenuItems

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React from "react";
-import Playground from "./components/playground";
+import Playground from "./components/Playground";
+import MenuItem from "./components/MenuItem";
 
 import "./App.css";
+
+const menuItems = ["Home", "ABout", "Trending Aticles", "Contact"];
 
 function App() {
   return (
     <div className="App">
       <Playground title="try" />
+      <ol>
+        {menuItems.map((title, i) => {
+          console.log(title, i);
+          return <MenuItem title={title} index={i + 1} />;
+        })}
+      </ol>
+
       <header className="App-header"></header>
     </div>
   );
 }
 
 export default App;
+
+// Step 1: Define an array of post objects (JSON)
+// Step 2: Map over that array like the menu item
+// Step 3: Make a post item component just like the menu item, make that look Be_you_ti_ful
+// Step 4: Survive the next 9 weeks
