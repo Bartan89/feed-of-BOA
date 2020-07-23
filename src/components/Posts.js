4b0295d0371cd7aabdefd5b523b3e5575614f7c6
AliLotfi123 <a.lotfi@outlook.com>
2020-07-23 20:53:20 +0200

    added topping filter

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,27 +1,40 @@
-import React, { useState, useEffect } from "react"
-import PizzaJson from "../data/pizza.json"
-import Post from "./Post"
+import React, { useState, useEffect } from "react";
+import PizzaJson from "../data/pizza.json";
+import Post from "./Post";
 
-export default function Posts() {
-  const [pizzas, set_pizzas] = useState(PizzaJson)
+export default function Posts(props) {
+  const [pizzas, set_pizzas] = useState(PizzaJson);
+  const [filter, setFilter] = useState([]);
+  const [ingredient, setIngredient] = useState("");
+  const [sorted, sortedSet] = useState("name");
 
-  const [sorted, sortedSet] = useState("name")
-
-  let newSort
+  let newSort;
   if (sorted === "name") {
-    newSort = pizzas.sort((a, b) => a.name.localeCompare(b.name))
+    newSort = pizzas.sort((a, b) => a.name.localeCompare(b.name));
   } else if (sorted === "worstToBest") {
-    newSort = pizzas.sort((a, b) => a.popularity - b.popularity)
+    newSort = pizzas.sort((a, b) => a.popularity - b.popularity);
   } else if (sorted === "bestToWorst") {
-    newSort = pizzas.sort((a, b) => b.popularity - a.popularity)
+    newSort = pizzas.sort((a, b) => b.popularity - a.popularity);
   } else if (sorted === "mostIngredients") {
-    newSort = pizzas.sort((a, b) => b.toppings.length - a.toppings.length)
+    newSort = pizzas.sort((a, b) => b.toppings.length - a.toppings.length);
   } else if (sorted === "leastIngredients") {
-    newSort = pizzas.sort((a, b) => a.toppings.length - b.toppings.length)
+    newSort = pizzas.sort((a, b) => a.toppings.length - b.toppings.length);
+  }
+
+  // console.log(props.ingredient);
+
+  function pizzaFilter() {
+    const filterdPizza = [...pizzas].filter((pizza) => {
+      // console.log(props.ingredient);
+      // console.log(pizza.toppings);
+      return pizza.toppings.includes(ingredient.toLowerCase());
+    });
+    // console.log(filterdPizza);
+    setFilter(filterdPizza);
   }
 
   function SortPizzas(event) {
-    sortedSet(event.target.value)
+    sortedSet(event.target.value);
   }
 
   if (pizzas === undefined) {
@@ -29,10 +42,21 @@ export default function Posts() {
       <div>
         <p>loading</p>
       </div>
-    )
+    );
   } else {
     return (
       <div>
+        <p>
+          <label htmlfor="cars">Favorite Topping:</label>
+          <select onChange={(event) => setIngredient(event.target.value)}>
+            <option value="Salami">Salami</option>
+            <option value="Ham">Ham</option>
+            <option value="Mushrooms">Mushrooms</option>
+
+            {/* <option value="mostIngredients">Most ingredients</option>
+          <option value="leastIngredients">Least ingredients</option> */}
+          </select>
+        </p>
         <label htmlfor="cars">Sort by:</label>
         <select onChange={SortPizzas}>
           <option value="name">Name</option>
@@ -41,10 +65,39 @@ export default function Posts() {
           <option value="mostIngredients">Most ingredients</option>
           <option value="leastIngredients">Least ingredients</option>
         </select>
-        {newSort.map(pizza => {
-          return <Post name={pizza.name} base={pizza.base} sauce={pizza.sauce} cheese={pizza.cheese} toppings={pizza.toppings} img={pizza.img} popularity={pizza.popularity} />
-        })}
+        <p>
+          <button onClick={pizzaFilter}>filter by ingrident</button>
+        </p>
+        {filter.length > 0
+          ? filter.map((pizza) => {
+              return (
+                <Post
+                  name={pizza.name}
+                  base={pizza.base}
+                  sauce={pizza.sauce}
+                  cheese={pizza.cheese}
+                  toppings={pizza.toppings}
+                  img={pizza.img}
+                  popularity={pizza.popularity}
+                  ingredient={ingredient}
+                />
+              );
+            })
+          : pizzas.map((pizza) => {
+              return (
+                <Post
+                  name={pizza.name}
+                  base={pizza.base}
+                  sauce={pizza.sauce}
+                  cheese={pizza.cheese}
+                  toppings={pizza.toppings}
+                  img={pizza.img}
+                  popularity={pizza.popularity}
+                  ingredient={ingredient}
+                />
+              );
+            })}
       </div>
-    )
+    );
   }
 }
